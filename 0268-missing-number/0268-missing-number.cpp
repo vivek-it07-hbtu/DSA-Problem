fc518ac75f483877fd4ae524325f369d035afed8
Vivek Chaudhary <vk449232@gmail.com>
2026-07-15 14:29:21 +0530

    Time: 0 ms (100%), Space: 22 MB (12.75%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        // Method 1 : 
+        // Method 1 : using basics math but interviewer reject you
         int n=nums.size();
-        int actualsum=0;
-        for(int i=0;i<n;i++){
-            actualsum+=nums[i];
+        // int actualsum=0;
+        // for(int i=0;i<n;i++){
+        //     actualsum+=nums[i];
+        // }
+        // int sum=n*(n+1)/2;
+        // int missingnumber= (sum-actualsum);
+        // return missingnumber;
+
+        // Mehtod 2 : Optimal solution
+        //a ^ a = 0       a ^ 0 = a
+        int ans = n;
+        for (int i = 0; i < n; i++) {
+            ans ^= i;
+            ans ^= nums[i];
         }
-        int sum=n*(n+1)/2;
-        int missingnumber= (sum-actualsum);
-        return missingnumber;
+        return ans;
     }
 };
