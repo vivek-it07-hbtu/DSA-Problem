1616abb9fd8a95c5bee0aaf1a6b8276335b356a5
Vivek Chaudhary <vk449232@gmail.com>
2026-07-25 09:55:03 +0530

    Time: 2 ms (50.7%), Space: 8.6 MB (37.71%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+
+        if (x < 0)
+            return false;
+
+        int original = x;
+        long long rev = 0;
+
+        while (x > 0) {
+            int digit = x % 10;
+            rev = rev * 10 + digit;
+            x /= 10;
+        }
+
+        return original == rev;
+    }
+};
