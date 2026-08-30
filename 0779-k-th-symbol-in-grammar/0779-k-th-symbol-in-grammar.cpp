0c4ef7640c35dfb568f5ee1815cb4ba6a5d01a8b
Vivek Chaudhary <vk449232@gmail.com>
2026-08-31 01:40:39 +0530

    Time: 0 ms (100%), Space: 7.9 MB (49.16%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int kthGrammar(int n, int k) {
+        if(n==1) return 0;
+        if(k%2==0){ //flip , k/2
+            int prevAns= kthGrammar(n-1,k/2);
+            if(prevAns==0) return 1;
+            else return 0;
+
+        }
+        else{ // k agar odd hoga toh same rhega k/2+1
+            int prevAns= kthGrammar(n-1,k/2+1);
+            return prevAns;
+        }
+    }
+};
