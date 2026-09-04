9ce8c597a2eda54aeee44974d2e7a88dd21f8b11
Vivek Chaudhary <vk449232@gmail.com>
2026-09-05 00:12:58 +0530

    Time: 0 ms (100%), Space: 21.7 MB (93.93%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,13 +3,12 @@ public:
     int missingNumber(vector<int>& nums) {
         // Method 1 : using basics math but interviewer reject you
         int n=nums.size();
-        // int actualsum=0;
-        // for(int i=0;i<n;i++){
-        //     actualsum+=nums[i];
-        // }
-        // int sum=n*(n+1)/2;
-        // int missingnumber= (sum-actualsum);
-        // return missingnumber;
+        int actualsum=n*(n+1)/2;
+        int sum = 0;
+        for(int i =0;i<n;i++){
+            sum += nums[i];
+        }
+        return actualsum-sum;
 
         // Mehtod 2 : Optimal solution
         //a ^ a = 0       a ^ 0 = a
@@ -21,16 +20,16 @@ public:
         // return ans;
 
         //cyclic Sort
-        int i = 0;
-        while(i<n){
-            int correctIdx=nums[i];
-            if(i==correctIdx || nums[i]==n) i++;
-            else swap(nums[i],nums[correctIdx]);
-        }
-        for(int i=0; i<n; i++){
-            if(nums[i]!=i) return i;
-        }
-        return n;
+        // int i = 0;
+        // while(i<n){
+        //     int correctIdx=nums[i];
+        //     if(i==correctIdx || nums[i]==n) i++;
+        //     else swap(nums[i],nums[correctIdx]);
+        // }
+        // for(int i=0; i<n; i++){
+        //     if(nums[i]!=i) return i;
+        // }
+        // return n;
     }
 
 };
