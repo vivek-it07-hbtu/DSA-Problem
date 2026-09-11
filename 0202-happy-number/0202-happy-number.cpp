66ac7b443d00877cbde284ce693a849ee1e66a67
Vivek Chaudhary <vk449232@gmail.com>
2026-09-12 01:14:24 +0530

    Time: 0 ms (100%), Space: 7.8 MB (67.02%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int fun(int n) {
+    int sum = 0;
+    while(n > 0) {
+        int digit = n % 10;
+            sum += digit * digit;
+            n = n / 10;
+        }
+        return sum;
+    }
+    bool isHappy(int n) {
+        int slow=n;
+        int fast=n;
+        while(fast !=1){
+            slow = fun(slow);
+            fast = fun(fast);
+            fast = fun(fast);
+            if(slow==fast && slow !=1){
+                return false;
+            }
+        }
+        return true;
+    }
+};
